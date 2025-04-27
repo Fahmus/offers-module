81e4922ebf828119d089390ec3c57232337b92c9
Fahmi Laajimi <fahmi.laajimi@capgemini.com>
2025-04-27 16:39:02 +0200

    feat: Implement Config model

---
+++ Model/Config.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types = 1);
+
+namespace Dnd\OffersBanner\Model;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
+
+/**
+ * Class Config
+ *
+ * This class provides configuration settings for the Offers Banner module.
+ */
+class Config
+{
+    /**
+     * Path to the configuration setting that enables the offers banner.
+     */
+    const CONFIG_PATH_ENABLED = 'catalog/offers_banner/enabled';
+
+    /**
+     * Config constructor.
+     *
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        private readonly ScopeConfigInterface $scopeConfig
+    ) {
+    }
+
+    /**
+     * Check if the offers banner is enabled.
+     *
+     * @param string $scopeType The scope type (default is store scope).
+     * @param string|null $scopeCode The scope code (optional).
+     * @return bool True if the offers banner is enabled, false otherwise.
+     */
+    public function isEnabled(string $scopeType = ScopeInterface::SCOPE_STORE, ?string $scopeCode = null): bool
+    {
+        return $this->scopeConfig->isSetFlag(self::CONFIG_PATH_ENABLED, $scopeType, $scopeCode);
+    }
+}

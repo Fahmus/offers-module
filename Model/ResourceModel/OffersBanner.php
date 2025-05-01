bc3a5b8ea676eb603152569d8dac19d5498ded1d
Fahmi Laajimi <fahmi.laajimi@capgemini.com>
2025-05-01 13:46:36 +0200

    feat: Implement OffersBanner resource model

---
+++ Model/ResourceModel/OffersBanner.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Dnd\OffersBanner\Model\ResourceModel;
+
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+
+/**
+ * Class OffersBanner resource model.
+ */
+class OffersBanner extends AbstractDb
+{
+    /**
+     * @inheritdoc
+     */
+    protected $_idFieldName = 'id';
+
+    /**
+     * @inheritDoc
+     */
+    protected function _construct(): void
+    {
+        $this->_init('offers_banner', $this->_idFieldName);
+    }
+}
